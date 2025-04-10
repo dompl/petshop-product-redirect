bd3a3df2a2d5fe13980d17a1758446e8158639e4
rfsdev server <ino@redfrogstudio.co.uk>
2025-04-10 08:24:44 +0000

    Include vendor for plugin ZIP install

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dompl/petshop-product-redirect',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '19ad7b9d991fb7d9c41946cc1b3293fa6af0b398',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'dompl/petshop-product-redirect' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '19ad7b9d991fb7d9c41946cc1b3293fa6af0b398',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'yahnis-elsts/plugin-update-checker' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '73e6281e434bceffa7884568611804f8c38e27b5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yahnis-elsts/plugin-update-checker',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
